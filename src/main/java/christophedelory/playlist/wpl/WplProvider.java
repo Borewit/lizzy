581012c8474a00ac79cec7ad7645dadd2a4ee1c3
Borewit <Borewit@users.noreply.github.com>
2023-03-16 20:01:34 +0100

    Convert WPL to JAXB

--- src/main/java/christophedelory/playlist/wpl/WplProvider.java
+++ src/main/java/christophedelory/playlist/wpl/WplProvider.java
@@ -26,10 +26,7 @@ package christophedelory.playlist.wpl;
 
 import christophedelory.content.type.ContentType;
 import christophedelory.player.PlayerSupport;
-import christophedelory.playlist.*;
-import christophedelory.xml.XmlSerializer;
-
-import java.io.InputStream;
+import christophedelory.playlist.smil.SmilProvider;
 
 /**
  * WPL (Windows Media Player Playlist) is a proprietary XML file format used in Microsoft Windows Media Player versions 9-11.
@@ -37,7 +34,7 @@ import java.io.InputStream;
  * @author Christophe Delory
  * @version $Revision: 91 $
  */
-public class WplProvider extends AbstractPlaylistProvider
+public class WplProvider extends SmilProvider
 {
     /**
      * A list of compatible content types.
@@ -66,94 +63,4 @@ public class WplProvider extends AbstractPlaylistProvider
         return FILETYPES.clone();
     }
 
-    @Override
-    public SpecificPlaylist readFrom(final InputStream in, final String encoding) throws Exception
-    {
-        // Unmarshal the WPL playlist.
-        final XmlSerializer serializer = XmlSerializer.getMapping("christophedelory/playlist/wpl"); // May throw Exception.
-        serializer.getUnmarshaller().setIgnoreExtraElements(false); // Force an error if unknown elements are found.
-
-        final Smil ret = (Smil) serializer.unmarshal(preProcessXml(in, encoding)); // May throw Exception.
-        ret.setProvider(this);
-
-        return ret;
-    }
-
-    @Override
-    public SpecificPlaylist toSpecificPlaylist(final Playlist playlist) throws Exception
-    {
-        final Smil ret = new Smil();
-        ret.setProvider(this);
-
-        final Body body = new Body();
-        ret.setBody(body);
-
-        addToPlaylist(body.getSeq(), playlist.getRootSequence()); // May throw Exception.
-
-        final Head header = new Head();
-        ret.setHeader(header);
-        final Meta meta = new Meta();
-        meta.setName("ItemCount");
-        meta.setContent(Integer.toString(body.getSeq().getMedias().size()));
-        header.addMeta(meta);
-
-        return ret;
-    }
-
-    /**
-     * Adds the specified generic playlist component, and all its childs if any, to the input sequence.
-     *
-     * @param wplSeq    the parent sequence. Shall not be <code>null</code>.
-     * @param component the generic playlist component to handle. Shall not be <code>null</code>.
-     */
-    private void addToPlaylist(final Seq wplSeq, final AbstractPlaylistComponent component)
-    {
-        if (component instanceof Sequence)
-        {
-            final Sequence sequence = (Sequence) component;
-
-            if (sequence.getRepeatCount() < 0)
-            {
-                throw new IllegalArgumentException("A WPL playlist cannot handle a sequence repeated indefinitely");
-            }
-
-            final AbstractPlaylistComponent[] components = sequence.getComponents();
-
-            for (int iter = 0; iter < sequence.getRepeatCount(); iter++)
-            {
-                for (AbstractPlaylistComponent c : components)
-                {
-                    addToPlaylist(wplSeq, c); // May throw Exception.
-                }
-            }
-        }
-        else if (component instanceof Parallel)
-        {
-            throw new IllegalArgumentException("A WPL playlist cannot play media at the same time");
-        }
-        else if (component instanceof christophedelory.playlist.Media)
-        {
-            final christophedelory.playlist.Media media = (christophedelory.playlist.Media) component;
-
-            if (media.getDuration() != null)
-            {
-                throw new IllegalArgumentException("A WPL playlist cannot handle a timed media");
-            }
-
-            if (media.getRepeatCount() < 0)
-            {
-                throw new IllegalArgumentException("A WPL playlist cannot handle a media repeated indefinitely");
-            }
-
-            if (media.getSource() != null)
-            {
-                for (int iter = 0; iter < media.getRepeatCount(); iter++)
-                {
-                    final Media wplMedia = new Media(); // NOPMD Avoid instantiating new objects inside loops
-                    wplMedia.setSource(media.getSource().toString());
-                    wplSeq.addMedia(wplMedia);
-                }
-            }
-        }
-    }
 }
