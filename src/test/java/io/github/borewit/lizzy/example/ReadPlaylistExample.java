a6027d46ea1a4738c8b961c8ac630ed87adbeaa3
Borewit <Borewit@users.noreply.github.com>
2023-03-24 10:05:50 +0100

    Add sample code

---
+++ src/test/java/io/github/borewit/lizzy/example/ReadPlaylistExample.java
@@ -0,0 +1,29 @@
+package io.github.borewit.lizzy.example;
+
+import io.github.borewit.lizzy.playlist.Media;
+import io.github.borewit.lizzy.playlist.SpecificPlaylist;
+import io.github.borewit.lizzy.playlist.SpecificPlaylistFactory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReadPlaylistExample
+{
+  public static void main(String[] args) throws IOException
+  {
+    Path playlistPath = Paths.get(System.getProperty("user.dir"),"samples", "asx", "test01.asx");
+    SpecificPlaylist specificPlaylist = SpecificPlaylistFactory.getInstance().readFrom(playlistPath);
+    if (specificPlaylist == null)
+    {
+      System.exit(-1);
+    }
+    specificPlaylist.toPlaylist().getRootSequence().getComponents().forEach(component -> {
+      if (component instanceof Media)
+      {
+        Media media = (Media) component;
+        System.out.printf("Media with content-source=%s\n", media.getSource().toString());
+      }
+    });
+  }
+}
